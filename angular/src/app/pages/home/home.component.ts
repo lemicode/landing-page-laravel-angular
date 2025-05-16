4e9c2ec981a89ab7d85bc529ad1ee4f20b44b5af
Marco Vanegas <marco@example.com>
2025-05-16 08:08:25 -0500

    Se ajustó el componente home

--- angular/src/app/pages/home/home.component.ts
+++ angular/src/app/pages/home/home.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  customerForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z\s]+$/)]),
+    lastName: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z\s]+$/)]),
+    identityNumber: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+$/)]),
+    departmentId: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+$/)]),
+    cityId: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+$/)]),
+    phoneNumber: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+$/)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    habeasDataConsent: new FormControl(false, [Validators.requiredTrue])
+  });
+
+  handleSubmit() {}
+
+  downloadExcel() {}
+
+  selectWinner() {}
+}
